fedc161238af1459c857955d4abf57a2b2261021
filwie <filip.wiechec@gmail.com>
2017-09-27 21:57:31 +0200

    Added initial version of setup script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,19 @@
 
 
 function main () {
+    local dotfiles_repo="https://github.com/philowisp/dotfiles"
+    local destination=~/.dotfiles
+
+    if [[ -f ${destination} ]];
+    then
+        rm -rf ${destination}
+    fi
+
+    git clone ${dotfiles_repo} ${destination} &&
+        for dotfile in ${destination}/dotfiles/*
+        do
+            echo "${dotfile}"
+        done
 }
 
-
+main
