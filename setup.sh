e2157bed5cee0594e077c55d042129b82d079f63
filwie <filip.wiechec@gmail.com>
2017-09-28 10:58:48 +0200

    Fixed creating symlinks

--- setup.sh
+++ setup.sh
@@ -5,14 +5,14 @@ function main () {
     local dotfiles_repo="https://github.com/philowisp/dotfiles"
     local destination=~/.dotfiles
 
-    [[ -d ${destination} ]] && rm -rf "${destination}"
+    [[ -d ${destination} ]] && rm -rf ${destination}
 
         git clone ${dotfiles_repo} ${destination} &&
         for file in ${destination}/dotfiles/*
         do
             local dotfile="~/.$(basename ${file})"
             [[ -f "${dotfile}" ]] && rm "${dotfile}"
-            ln "${file}" "${dotfile}"
+            ln "${dotfile}" "${file}"
         done
 }
 
