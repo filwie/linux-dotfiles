ca7036b23dbfdf99ecd05f12b6cf8a9552ad16ec
filwie <filip.wiechec@gmail.com>
2017-10-03 12:52:57 +0200

    Added installing Vundle if it is not present

--- setup.sh
+++ setup.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+
+function install_vundle_if_needed () {
+    local vundle_path=~/.vim/bundle/Vundle.vim
+    [[ -f "${vundle_path}" ]] && git clone \
+        https://github.com/VundleVim/Vundle.vim.git "${vundle_path}"
+}
+
+
 function main () {
     local dotfiles_repo="https://github.com/philowisp/dotfiles"
     local destination=~/.dotfiles
@@ -9,6 +17,8 @@ function main () {
     if [[ -d ${destination} ]]; then
         rm -rf ${destination}
     fi
+    #TODO: install vim, tmux,zsh if not installed, along with oh-my-zsh
+    install_vundle_if_needed
 
     git clone ${dotfiles_repo} ${destination} &&
     for file in ${destination}/dotfiles/*
