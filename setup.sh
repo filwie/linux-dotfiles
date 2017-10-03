66ad572e6f33d5cd8a9d4c7f76cb59393a2f5e61
filwie <filip.wiechec@gmail.com>
2017-10-03 14:37:31 +0200

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ set -e
 function install_vundle_if_needed () {
     local vundle_path=~/.vim/bundle/Vundle.vim
     if ! [[ -d ${vundle_path} ]]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git "${vundle_path}"
+    git clone https://github.com/VundleVim/Vundle.vim.git ${vundle_path}
     fi
 }
 
