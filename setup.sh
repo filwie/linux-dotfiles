f45f3854a3f837fb29b23b69dedfa37251b047ed
filwie <filip.wiechec@gmail.com>
2018-06-05 10:23:34 +0200

    Add installing tmux plugin manager

--- setup.sh
+++ setup.sh
@@ -52,6 +52,7 @@ function main () {
     tic -x ${destination}/utils/xterm-256color-italic.terminfo
     tic -x ${destination}/utils/tmux-256color.terminfo
     wget https://raw.githubusercontent.com/Valloric/ycmd/3ad0300e94edc13799e8bf7b831de8b57153c5aa/cpp/ycm/.ycm_extra_conf.py ~/.ycm_extra_conf.py 
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 }
 
 main
