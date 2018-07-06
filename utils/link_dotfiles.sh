7f7ade0b4c374457358d341d01df939185e4cbdd
filwie <filip.wiechec@gmail.com>
2018-07-06 11:04:53 +0200

    Add symlinking .config dir aswell

--- utils/link_dotfiles.sh
+++ utils/link_dotfiles.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 dotfiles_dir=~/.dotfiles/dotfiles
+config_dir=~/.dotfiles/dotfiles/config/
 
 find ${dotfiles_dir} -maxdepth 1 -type f | while read -r file
 do
@@ -10,4 +11,17 @@ do
     [[ "$(uname -a)" = *"Darwin"* ]] \
         && ln "${dotfile_in_repo}" "${dotfile}" && echo "Created hard link to ${dotfile}" \
         || ln -s "${dotfile_in_repo}" "${dotfile}" && echo "Created a soft link to ${dotfile}"
+
 done
+
+if [[ ! "$(uname -a)" = *"Darwin"* ]]; then
+    ls -1 ${config_dir} | while read -r file
+    do
+        config_in_repo="${config_dir}/${file}"
+        config=~/.config/"${file}"
+        [ -e "${config}" ] && rm -rf ${config} \
+            && echo "Removed old ${config}" \
+            || echo "${config} not present"
+        ln -s "${config_in_repo}" "${config}" && echo "Symlinked ${config}"
+    done
+fi
