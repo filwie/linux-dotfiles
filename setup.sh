9db08fd26ddc45936ea9eedba447f90337618f2f
filwie <filip.wiechec@gmail.com>
2018-07-29 11:26:03 +0200

    Upgrade setup script

--- setup.sh
+++ setup.sh
@@ -12,11 +12,36 @@ function install_vundle_if_needed () {
     fi
 }
 
+function install_packages () {
+    packages=("zsh" "tmux")
+    local package_manager="none"
+    [[ "$(uname -a)" = *"ARCH"* ]] && echo kek
+
+    [[ package_manager -eq "none" ]] \
+        && echo "Could not detect package manager." \
+        "Please install below packages manually." \
+        && echo 
+
+
+    [[ -d ${HOME}/.oh-my-zsh ]] \
+        || git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+    }
+
+
+
+install_packages
+
+function install_packages () {
+    local package_manager="none"
+    [[ "$(uname -a)" = *"ARCH"* ]] && echo kek
+
+    [[ package_manager = ""
+}
 
 function main () {
     local dotfiles_repo="git@gitlab.com:filip.wiechec/dotfiles.git"
     local destination=~/.dotfiles
-    
+
     cd
     if [[ -d ${destination} ]]; then
         rm -rf ${destination}
@@ -26,19 +51,18 @@ function main () {
 
     git clone ${dotfiles_repo} ${destination}
 
-    bash "${destination}/utils/link_dotfiles.sh"
+    sh "${destination}/utils/link_dotfiles"
 
     [[ -d ~/bin ]] || mkdir ~/bin
-    ln -s "${destination}/utils/link_dotfiles.sh" ~/bin/link_dotfiles
+    ln -s "${destination}/utils/link_dotfiles" ~/bin/link_dotfiles
 
     vim +PluginInstall +qall
     vim +GoInstallBinaries +qall
-    echo "Installing below packages in used virtualenv might be required"
-    echo -e "- flake8\\n- pylint\\n- autopep8"
-    
-    tic -x ${destination}/utils/xterm-256color-italic.terminfo
+
+    tic -x ${destination}/utils/xterm-256color-italic.terminfo \
+        &&
     tic -x ${destination}/utils/tmux-256color.terminfo
-    wget https://raw.githubusercontent.com/Valloric/ycmd/3ad0300e94edc13799e8bf7b831de8b57153c5aa/cpp/ycm/.ycm_extra_conf.py ~/.ycm_extra_conf.py 
+    wget https://raw.githubusercontent.com/Valloric/ycmd/3ad0300e94edc13799e8bf7b831de8b57153c5aa/cpp/ycm/.ycm_extra_conf.py ~/.ycm_extra_conf.py
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 }
 
