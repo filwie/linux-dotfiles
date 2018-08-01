b16da8bf6fdbe495a29f93ae30efefad6e43ae45
filwie <filip.wiechec@gmail.com>
2018-08-01 15:46:08 +0200

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
-
-function install_vundle_if_needed () {
-    local branch=master
-    local vundle_path=~/.vim/bundle/Vundle.vim
-    if ! [[ -d ${vundle_path} ]]; then
-    timeout 10 git clone git@gitlab.com:filip.wiechec/dotfiles.git ${vundle_path} -b ${branch} || \
-        git clone https://gitlab.com/filip.wiechec/dotfiles.git ${vundle_path} -b ${branch}
-    fi
-}
-
 function install_packages () {
     packages=("zsh" "tmux")
     local package_manager="none"
@@ -20,50 +8,43 @@ function install_packages () {
     [[ package_manager -eq "none" ]] \
         && echo "Could not detect package manager." \
         "Please install below packages manually." \
-        && echo 
+        && echo
 
 
     [[ -d ${HOME}/.oh-my-zsh ]] \
         || git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
     }
 
-
-
-install_packages
-
-function install_packages () {
-    local package_manager="none"
-    [[ "$(uname -a)" = *"ARCH"* ]] && echo kek
-
-    [[ package_manager = ""
-}
-
 function main () {
     local dotfiles_repo="git@gitlab.com:filip.wiechec/dotfiles.git"
     local destination=~/.dotfiles
 
     cd
+
     if [[ -d ${destination} ]]; then
+        echo "Existing .dotfiles directory found. Removing..."
         rm -rf ${destination}
     fi
-    #TODO: install vim, tmux,zsh if not installed, along with oh-my-zsh
-    install_vundle_if_needed
-
+    echo -e "\nCloning dotfiles repo..."
     git clone ${dotfiles_repo} ${destination}
 
+    echo -e "\nRunning script linking dotfiles..."
     sh "${destination}/utils/link_dotfiles"
 
+    echo -e "\nValidating ~/bin directory exists..."
     [[ -d ~/bin ]] || mkdir ~/bin
+
+    echo -e "\n Linking linking script itself"
     ln -s "${destination}/utils/link_dotfiles" ~/bin/link_dotfiles
 
-    vim +PluginInstall +qall
-    vim +GoInstallBinaries +qall
-
+    echo -e "\nAdding terminfo profiles..."
     tic -x ${destination}/utils/xterm-256color-italic.terminfo \
-        &&
-    tic -x ${destination}/utils/tmux-256color.terminfo
-    wget https://raw.githubusercontent.com/Valloric/ycmd/3ad0300e94edc13799e8bf7b831de8b57153c5aa/cpp/ycm/.ycm_extra_conf.py ~/.ycm_extra_conf.py
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+        && tic -x ${destination}/utils/tmux-256color.terminfo \
+        || echo -e "\eUnable to load .terminfo files - do it manually\n\ttic -x ${destination}/utils/xterm-256color-italic.terminfo\n\ttic -x ${destination}/utils/tmux-256color.terminfo \n"
+
+    echo -e "\nInstalling tmux plugin manager..."
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm \
+        && echo -e "\n\nPress C-a I to install tmux plugins!"
 }
 
 main
