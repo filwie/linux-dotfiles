6f4a7ae03779cc84517c65666d8337b95e13a622
filwie <filip.wiechec@gmail.com>
2018-12-17 16:13:21 +0100

    [i3] i3 config merger refactor

--- bin/i3-merge-conf.sh
+++ bin/i3-merge-conf.sh
@@ -9,17 +9,13 @@ merge_conf () {
     local i3_config_x240=${i3_config_dir}/x240
     local i3_config_workstation=${i3_config_dir}/workstation
 
-    local i3_config_pure_i3="${i3_config_dir}/pure_i3"
+    local i3_config_multimedia_keys="${i3_config_dir}/multimedia_keys"
 
-    local configs_to_merge=("${i3_config_base}" "${i3_config_status}")
+    local configs_to_merge=("${i3_config_base}" "${i3_config_status}" "${i3_config_multimedia_keys}")
 
     [[ $(hostname) = *"x240"* ]] \
         && configs_to_merge+="${i3_config_x240}" \
         || configs_to_merge+="${i3_config_workstation}"
-
-    [[ "${PURE_I3}" == "true" ]] \
-        || configs_to_merge+="${i3_config_xfce4_integration}" \
-        && configs_to_merge+="${i3_config_pure_i3}"
 
     cat ${configs_to_merge[@]} > ${i3_config} \
         && echo "Config files: ${configs_to_merge[@]} succesfully merged."
