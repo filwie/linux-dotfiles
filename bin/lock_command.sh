f4df3a4c2f2189d825c36a2eee5e19ded1ca0f16
filwie <filip.wiechec@gmail.com>
2018-12-17 16:16:08 +0100

    Refactor

--- bin/lock_command.sh
+++ bin/lock_command.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+set -e
 
 background_image="${HOME}/Pictures/lock.png"
 tmp_image="/tmp/screen_capture.png"
+lock_icons=("/usr/share/icons/Paper/512x512/actions/lock.png")
+
+num_of_displays=$(xrandr -q | grep ' connected' | wc -l)
 
 function create_background_image () {
     scrot "${tmp_image}" -quality "1"
@@ -10,17 +14,22 @@ function create_background_image () {
     eval "${convert_cmd} ${tmp_image} ${background_image}"
 }
 
-function lock_screen () {
-    i3lock -ti "${background_image}"
-}
-
-
-function main () {
+function set_background () {
     if [[ -f "${background_image}" ]]; then
         create_background_image &
     else
         create_background_image
     fi
+
+}
+
+function lock_screen () {
+    exec i3lock -ti "${background_image}"
+}
+
+
+function main () {
+    set_background
     lock_screen
 }
 
