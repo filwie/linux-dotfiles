d90ef6b90a7a5298920fff172d8b5fce63bd5fc8
filwie <filip.wiechec@gmail.com>
2018-12-24 12:33:43 +0100

    Alacritty font switching script initial version

--- bin/alacritty_font.sh
+++ bin/alacritty_font.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
+set -e
 
+repo_dir="${HOME}/.dotfiles"
+setup_script="${repo_dir}/setup.sh"
+tmp_setup="/tmp/setup.sh"
 
+# source the setup script but do not call main function (last line)
+cat ${setup_script} | head -n -1 > "${tmp_setup}"
+source "${tmp_setup}"
+
+verbosity=3
+
+debug_msg "TODO: sed magick and all the stuff"
