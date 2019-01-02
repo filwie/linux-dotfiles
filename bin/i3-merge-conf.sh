2eeef404f535bbc5ef02a11f9a17be866b250a41
filwie <filip.wiechec@gmail.com>
2019-01-02 20:09:16 +0100

    [i3] Restructure how config files are split

--- bin/i3-merge-conf.sh
+++ bin/i3-merge-conf.sh
@@ -1,21 +1,40 @@
 #!/usr/bin/env zsh
+
 merge_conf () {
 
     local i3_config_dir="${HOME}/.config/i3"
 
-    local i3_config=${i3_config_dir}/config
-    local i3_config_base=${i3_config_dir}/base
-    local i3_config_status=${i3_config_dir}/status_bar
-    local i3_config_x240=${i3_config_dir}/x240
-    local i3_config_workstation=${i3_config_dir}/workstation
+    local i3_config="${i3_config_dir}/config"
+    local i3_config_base="${i3_config_dir}/base"
+
+    local i3_config_variables="${i3_config_dir}/variables.common"
+    local i3_config_variables_x240="${i3_config_dir}/variables.x240"
+    local i3_config_variables_workstation="${i3_config_dir}/variables.workstation"
+
+    local i3_config_autostart="${i3_config_dir}/autostart.common"
+    local i3_config_autostart_x240="${i3_config_dir}/autostart.x240"
+    local i3_config_autostart_workstation="${i3_config_dir}/autostart.workstation"
 
     local i3_config_multimedia_keys="${i3_config_dir}/multimedia_keys"
 
-    local configs_to_merge=("${i3_config_base}" "${i3_config_status}" "${i3_config_multimedia_keys}")
+    local configs_to_merge=(
+        "${i3_config_base}"
+        "${i3_config_variables}"
+        "${i3_config_autostart}"
+        "${i3_config_multimedia_keys}"
+    )
 
-    [[ $(hostname) = *"x240"* ]] \
-        && configs_to_merge+="${i3_config_x240}" \
-        || configs_to_merge+="${i3_config_workstation}"
+    if [[ $(hostname) = *"x240"* ]]; then
+        configs_to_merge+=(
+            "${i3_config_variables_x240}"
+            "${i3_config_autostart_x240}"
+        )
+    else
+        configs_to_merge+=(
+            "${i3_config_variables_workstation}"
+            "${i3_config_autostart_workstation}"
+        )
+    fi
 
     cat ${configs_to_merge[@]} > ${i3_config} \
         && echo "Config files: ${configs_to_merge[@]} succesfully merged."
