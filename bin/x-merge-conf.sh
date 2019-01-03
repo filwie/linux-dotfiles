9da6da6407571835e8441db4f0c907e3c52761dc
filwie <filip.wiechec@gmail.com>
2019-01-03 16:26:26 +0100

    Rename xresources include files to match other cases in repo

--- bin/x-merge-conf.sh
+++ bin/x-merge-conf.sh
@@ -1,15 +1,18 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
+set -e
+
 merge_conf () {
 
     local xresources=${HOME}/.Xresources
-    local xresources_base=${HOME}/.Xresources_base
-    local xresources_hidpi=${HOME}/.Xresources_hidpi
+    local xresources_base=${HOME}/.Xresources.common
+    local xresources_hidpi=${HOME}/.Xresources.x240
 
 
     [[ $(hostname) = *"x240"* ]] \
         && cat ${xresources_base} ${xresources_hidpi} > ${xresources} \
         || cat ${xresources_base} > ${xresources}
 
+    echo "Merged .Xresources"
     }
 
 merge_conf
